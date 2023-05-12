13ea13a72f57da0e0fb27a2e64666ebb84b6dee6
I0337516 <lea.hodac@insel.ch>
2023-05-12 17:46:05 +0200

    change curve, previous and next, add data.
    Next: layout, automatic lasso, curve and not scatter

--- myApp.py
+++ myApp.py
@@ -7,9 +7,8 @@ import pickle
 import pandas as pd
 import plotly.express as px
 import json
-import numpy as np
 
-
+#https://www.google.com/search?q=position+on+the+page+dash+app&oq=position+on+the+page+dash+app&aqs=edge..69i57.5640j0j1&sourceid=chrome&ie=UTF-8#fpstate=ive&vld=cid:d062947d,vid:MtSgh6FOL7I
 # incorporate data into app
 def load_table_pickle(path, name_file):
     file = path + "\\" + name_file + '_rawCurves.pkl'
@@ -20,46 +19,53 @@ def load_table_pickle(path, name_file):
     print("... done loading dictionary from file", file)
     return df_
 
-
 root = r'\\filer300\USERS3007\I0337516\Desktop\spiroQC_project\XBJC_set'
 name_file_df_data = 'format_df_data_from_XML'
-data = load_table_pickle(root, name_file_df_data)
-df = data[['vol_FV', 'flow_FV']]
-flow = df.at[0, 'flow_FV']
-vol = df.at[0, 'vol_FV']
-df = pd.DataFrame({
-    "x": vol,
-    "y": flow
-})
+data_curves = load_table_pickle(root, name_file_df_data)
+df_curves = data_curves[['vol_FV', 'flow_FV']]
 
-fig = px.scatter(df, x="x", y="y", width=800, height=800)
-fig.update_layout(clickmode='event+select')
-
-currentdatetime = dt.datetime.now()
 
 app = dash.Dash(__name__, suppress_callback_exceptions=True)
 
+
 app.layout = html.Div([
 
-    dcc.Dropdown(['Artefact', 'Cough', 'Irregularity', 'Not defined'], 'Not defined', id='demo-dropdown'),
-    dcc.Graph(
-        id='flow_vol_curve',
-        figure=fig
-    ),
+    html.Div([
+        html.Div([
+            html.Pre(children='Curve index:'),
+        ], style={'display': 'inline-block'}),
 
-    html.Pre(id='selected-data'),
+        html.Div([
+            html.Pre(id='curveIndex'),
+        ], style={'display': 'inline-block'}),
+
+        html.Div([
+            html.Button('Previous curve', id='previous_curve', n_clicks=0),
+            html.Button('Next curve', id='next_curve', n_clicks=0),
+        ]),
+
+        dcc.Graph(
+            id='flow_vol_curve',
+            # figure=fig
+        ),
+    ], style={'display': 'inline-block'}),
 
     html.Div([
-        html.Button('add Selected Data', id='adding-data-button', n_clicks=0)
-    ]),
+        dcc.Dropdown(['Artefact', 'Cough', 'Irregularity', 'Not defined'], 'Not defined', id='demo-dropdown'),
+
+        html.Pre(id='selected-data'),
+
+        html.Button('add Selected Data', id='adding-data-button', n_clicks=0),
+
+        dash_table.DataTable(id='abnormalities-idx-tab'),
 
 
-    dash_table.DataTable(id='abnormalities-idx-tab'),
 
-    html.Button('Export to Excel', id='save_to_csv', n_clicks=0),
+        html.Button('Export to Excel', id='save_to_csv', n_clicks=0),
+        # Create notification when saving to excel
+        html.Div(id='placeholder', children=[]),
+    ], style={'display': 'inline-block'}),
 
-    # Create notification when saving to excel
-    html.Div(id='placeholder', children=[]),
     dcc.Store(id="store", data=0),
     dcc.Interval(id='interval', interval=1000),
 
@@ -67,12 +73,45 @@ app.layout = html.Div([
 
 
 # ------------------------------------------------------------------------------------------------
-# @app.callback(
-#     Output('abnormalities-idx-tab', 'Type'),
-#     Input('demo-dropdown', 'value')
-# )
-# def update_output(value):
-#     return f'You have selected {value}'
+
+
+@app.callback(
+    Output(component_id='curveIndex', component_property='children'),
+    Input(component_id='previous_curve', component_property='n_clicks'),
+    Input(component_id='next_curve', component_property='n_clicks'),
+    State(component_id='curveIndex', component_property='children'),
+)
+def updateIndex(prev_clicks, next_clicks, index):
+    """
+    When the next or previous buttons are clicked, update the image index
+    ensuring it stays within 0 <= index < len(contents)
+    """
+    if index is None:
+        index = 0
+    # get which button click triggered callback
+    trig = dash.callback_context.triggered[0]['prop_id']
+
+    # update data if necessary and return it
+    if trig == "previous_curve.n_clicks" and index > 0:
+        index = index - 1
+    elif trig == "next_curve.n_clicks" and index < len(df_curves) - 1:
+        index = index + 1
+
+    return index
+
+
+@app.callback(
+    Output(component_id='flow_vol_curve', component_property='figure'),
+    Input(component_id='curveIndex', component_property='children'),
+)
+def display_curve(n_clicks):
+    df_curve = pd.DataFrame({
+        "x": df_curves.at[n_clicks, 'vol_FV'],
+        "y": df_curves.at[n_clicks, 'flow_FV']
+    })
+    fig = px.scatter(df_curve, x="x", y="y", width=800, height=800)
+    fig.update_layout(clickmode='event+select')
+    return fig
 
 
 @app.callback(
@@ -80,23 +119,26 @@ app.layout = html.Div([
     Input(component_id='adding-data-button', component_property='n_clicks'),
     State(component_id='flow_vol_curve', component_property='selectedData'),
     State(component_id='demo-dropdown', component_property='value'),
+    State(component_id='curveIndex', component_property='children'),
     State(component_id='abnormalities-idx-tab', component_property='data'),
 )
-def add_rows_select(n_clicks, selectedData, abno_type, existing_rows):
+def add_rows_select(n_clicks, selectedData, abno_type, curve_idx, existing_rows):
 
     if n_clicks > 0:
-        if not selectedData is None:
+        if not (selectedData is None or len(selectedData["points"]) == 0):
             value_start = selectedData["points"][0]['pointIndex']
             value_end = selectedData["points"][-1]['pointIndex']
 
             if existing_rows is None:
                 existing_rows = ({
+                    'Curve index': curve_idx,
                     'Abnormality Type': abno_type,
                     'Start Index': value_start,
                     'Stop Index': value_end}),
 
             else:
                 existing_rows.append({
+                    'Curve index': curve_idx,
                     'Abnormality Type': abno_type,
                     'Start Index': value_start,
                     'Stop Index': value_end}),
@@ -108,12 +150,22 @@ def add_rows_select(n_clicks, selectedData, abno_type, existing_rows):
 
 
 
+# @app.callback(
+#     Output(component_id='flow_vol_curve', component_property='selectedData'),
+#     Input(component_id='previous_curve', component_property='n_clicks'),
+#     Input(component_id='next_curve', component_property='n_clicks'),
+# )
+# def clear_selected_data_change_fig(prev_n_clicks, next_n_clicks):
+#     return None
+
+
 @app.callback(
     Output(component_id='selected-data', component_property='children'),
     Input(component_id='flow_vol_curve', component_property='selectedData'))
 def display_selected_data(selectedData):  # selectedData is a property of Graph
-    if selectedData is None:
-        res = dict()
+
+    if selectedData is None or len(selectedData["points"]) == 0:
+        res = 'No data selected yet'
     else:
         first_index = selectedData["points"][0]
         last_index = selectedData["points"][-1]
